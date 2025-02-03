9e05e19ad09bf1e66db0d64fc4eb54e8c7a1a2e6
Abhishek <abhishek.kumar@quectosys.com>
2025-02-03 11:48:55 +0530

    final change

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HodController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\HodController;
 use App\Http\Controllers\MarksController;
+use App\Http\Controllers\NoticeController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 
@@ -44,17 +45,23 @@ Route::get('users', [UserController::class, 'index']);
 Route::post('users', [UserController::class, 'store']); // Add new user
 Route::put('users/{user}', [UserController::class, 'update']); // Update user
 Route::delete('users/{user}', [UserController::class, 'destroy']); // Delete user
-     
+
 Route::get('student', [StudentController::class, 'index']);
-Route::post('student', [StudentController::class, 'store']); 
-Route::post('student/{id}', [StudentController::class, 'update']); 
-Route::delete('student/{id}', [StudentController::class, 'destroy']); 
+Route::post('student', [StudentController::class, 'store']);
+Route::post('student/{id}', [StudentController::class, 'update']);
+Route::delete('student/{id}', [StudentController::class, 'destroy']);
 Route::get('roles/{roleId}/users', [UserController::class, 'getUsersByRole']);
 
 Route::get('marks', [MarksController::class, 'index']);
 Route::get('marks/{id}', [MarksController::class, 'show']);
-Route::post('marks', [MarksController::class, 'store']); 
-Route::post('marks/{id}', [MarksController::class, 'update']); 
-Route::delete('marks/{id}', [MarksController::class, 'destroy']); 
-     // Assign role to user
- Route::post('/assign-role', [AuthController::class, 'assignRole']);
+Route::post('marks', [MarksController::class, 'store']);
+Route::post('marks/{id}', [MarksController::class, 'update']);
+Route::delete('marks/{id}', [MarksController::class, 'destroy']);
+// Assign role to user
+Route::post('/assign-role', [AuthController::class, 'assignRole']);
+
+
+Route::get('/notices', [NoticeController::class, 'index']);
+Route::post('/notices', [NoticeController::class, 'store']);
+Route::get('/notices/{id}/download', [NoticeController::class, 'download']);
+Route::delete('/notices/{id}', [NoticeController::class, 'deleteNotice']);
