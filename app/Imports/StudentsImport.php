f56367063f9db8a6ec8c7b8757b73a1e479f9097
KRISHNA RAI <krishna@quectosys.com>
2025-05-13 10:16:45 +0530

    correct the row of student import

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -11,6 +11,9 @@ class StudentsImport implements ToModel
 {
     public function model(array $row)
     {
+         if (count($row) < 8) {
+        return null; // skip this row
+    }
         return new Student([
             'name' => $row[0],
             'roll_no' => $row[1],
@@ -18,6 +21,8 @@ class StudentsImport implements ToModel
             'phone' => $row[3],
             'branch' => $row[4],
             'session' => $row[5],
+            'parent_email' => $row[6],
+            'parent_phone' => $row[7],
         ]);
     }
 }
